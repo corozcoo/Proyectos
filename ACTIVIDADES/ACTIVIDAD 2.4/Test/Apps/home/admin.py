865bcd2efeb53080908afc9fdf445f3274efd559
corozcoo <87343975+corozcoo@users.noreply.github.com>
2021-09-10 19:43:21 -0600

    Actividad 2.4 Upload

--- ACTIVIDADES/ACTIVIDAD 2.4/Test/Apps/home/admin.py
+++
@@ -1,7 +0,0 @@
-from Apps.home.models import Cliente
-from django.contrib import admin
-from .models import Cliente, TipoCliente
-
-# Register your models here.
-admin.site.register(Cliente)
-admin.site.register(TipoCliente)
